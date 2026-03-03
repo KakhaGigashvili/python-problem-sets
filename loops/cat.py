75231a4ad519fb301b2e86c2c1bd3c5c5137950a
kakha gigashvili <151166994+KakhaGigashvili@users.noreply.github.com>
2026-03-03 15:41:14 +0000

    meal

---
+++ loops/cat.py
@@ -0,0 +1,55 @@
+# i = 3
+
+# while i != 0:
+#     print("meow")
+#     i -= 1
+#     # i = i - 1
+
+# i = 0
+
+# while i < 3:
+#     print("meow")
+#     # i = i + 1
+#     i += 1
+
+# lst = [0, 1, 2]
+
+# for i in len:
+#     print("meow")
+
+# for _ in range(3):
+#     print("meow")
+
+# print("meow\n" * 3, end="")
+
+# while True:
+#     n = int(input("what's n? "))
+#     if n > 0:
+#         break
+
+# for _ in range(n):
+#     print("meow")
+        
+
+# def main():
+#     meow(get_number())
+
+# def get_number():
+#     while True:
+#         n = int(input("What's n? "))
+#         if n >= 1:
+#             return n
+
+# def meow(n):
+#     for _ in range(n):
+#         print("meow")
+
+# main()
+
+
+# list = [0, 1, 2, 3, 4, 5]
+# i = 0
+
+# while i < len(list):
+#     print("meow")
+#     i += 1
