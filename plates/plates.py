b44e08eccf1b691aeb54c368303895b174843438
kakha gigashvili <151166994+KakhaGigashvili@users.noreply.github.com>
2026-03-11 20:20:52 +0000

    plates

---
+++ plates/plates.py
@@ -0,0 +1,28 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if len(s) < 2 or len(s) > 6:
+        return False
+    if not s[0].isalpha() or not s[1].isalpha():
+        return False
+    for i in range(len(s)):
+        if s[i].isdigit():
+             
+            if s[i] == "0":
+                return False
+
+            for j in range(i, len(s)):
+                if not s[j].isdigit():
+                    return False
+
+            break
+
+    return True
+
+main()
