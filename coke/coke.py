d9d5180637e6e1c87e8de16b7c41bdfb70c644a6
kakha gigashvili <151166994+KakhaGigashvili@users.noreply.github.com>
2026-03-12 19:34:22 +0000

    coke

---
+++ coke/coke.py
@@ -0,0 +1,11 @@
+coke = 50
+
+while coke > 0:
+    print("Amount Due:", coke)
+    coin = int(input("Insert Coin: "))
+
+    if coin in [25, 10, 5]:
+        coke -= coin
+
+change = -coke
+print("Change Owed:", change)
