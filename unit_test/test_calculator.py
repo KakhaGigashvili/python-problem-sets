498c402415ea44f5e236342c49d98c49d7ccaa98
kakha gigashvili <151166994+KakhaGigashvili@users.noreply.github.com>
2026-04-28 08:46:29 +0000

    changed test_calculator.py

--- unit_test/test_calculator.py
+++ unit_test/test_calculator.py
@@ -1,23 +1,23 @@
 from calculator import square
 
 def main():
-    test_square()
+
     test_positive()
-    test_negetive()
+    test_negative()
     test_zero()
  
-def test_square():
 
-    def test_positive():
-        assert square(2) == 4
-        assert square(3) == 9
 
-    def test_negative():
-        assert square(-2) == 4
-        assert square(-3) == 9
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
 
-    def test_zero():
-        assert square(0) == 0
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+def test_zero():
+    assert square(0) == 0
  
 if __name__ == "__main__":
     main()
