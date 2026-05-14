0f1f0ef8f6e4c9c99d2ab5f0bd7496198a1e440f
kakha gigashvili <151166994+KakhaGigashvili@users.noreply.github.com>
2026-05-14 17:31:14 +0000

    add ex1.py

---
+++ oop/ex/ex1.py
@@ -0,0 +1,28 @@
+class Restaurant:
+
+    def __init__(self, restaurant_name, cuisine_type):
+        
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+
+        result = f"{self.restaurant_name} {self.cuisine_type}"
+        print (result.title())
+
+    def open_restaurant(self):
+
+        massage = f"{self.restaurant_name} is open"
+
+        print(massage)
+    
+restaurant_ita = Restaurant("Kanto", "Italian Kitchen")
+restaurant_asian = Restaurant("mushu", "asian kitchen")
+
+
+restaurant_asian.describe_restaurant()
+restaurant_asian.open_restaurant()
+
+
+restaurant_ita.describe_restaurant()
+restaurant_ita.open_restaurant()
