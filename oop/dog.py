e4e0f22afcd018e2bf346dfd1b7f62b12310ec28
kakha gigashvili <151166994+KakhaGigashvili@users.noreply.github.com>
2026-05-14 17:35:25 +0000

    dog.py

---
+++ oop/dog.py
@@ -0,0 +1,16 @@
+class Dog():
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    
+my_dog = Dog("willie", 6)
+your_dog = Dog("bajo", 3)
+
+
+print(my_dog.name)
+print(my_dog.age)
+
+print(your_dog.name)
+print(your_dog.age)
